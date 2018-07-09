0c3466c7eb1bb4828a23f257131b522fc0560b74
Chris Stelzmueller <tuesd4y@protonmail.ch>
2018-07-09 23:07:26 +0200

    Initial commit
    Moved entity classes from wcapi project to here

---
+++ src/test/kotlin/at/triply/wcmodel/Test.kt
@@ -0,0 +1,11 @@
+package at.triply.wcmodel
+
+import org.junit.Test
+
+class Test {
+    //TODO: Test converting stuff from and to json with hardcoded json from wc rest service
+    @Test
+    fun testNothing() {
+        println("yes")
+    }
+}
